cb456283e980ec52e3136dc88c1dc1a48ac24c71
claufg <168189721+claufg@users.noreply.github.com>
2024-05-22 13:21:26 +0200

    Update Rectangle3D.py

--- Rectangle3D.py
+++ Rectangle3D.py
@@ -1,20 +1,21 @@
-from AbstractPolygon import AbstractPolygon
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-class Rectangle3D(AbstractPolygon):
-    def __init__(self, width, height, position=None, angles=None):
-        super().__init__(position, angles)
-        self.width = width
-        self.height = height
+class Rectangle(Form2D):
+    # Código existente...
 
-    def get_dimensions(self):
-        return self.width, self.height
+    def plot_3d(self, ax=None):
+        if ax is None:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+        points = np.array([[0, 0, 0], [0, self.width, 0], [self.length, self.width, 0], [self.length, 0, 0]])
+        verts = [list(zip(points[:, 0], points[:, 1], points[:, 2]))]
+        ax.add_collection3d(Poly3DCollection(verts, alpha=.25, linewidths=1, edgecolors='r'))
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        ax.set_title('3D Representation of Rectangle')
+        ax.set_xlim([0, self.length])
+        ax.set_ylim([0, self.width])
+        ax.set_zlim([0, max(self.length, self.width)])
 
-    def set_dimensions(self, width, height):
-        self.width = width
-        self.height = height
-
-    def __eq__(self, other):
-        return isinstance(other, Rectangle3D) and super().__eq__(other) and self.width == other.width and self.height == other.height
-
-    def __str__(self):
-        return f"{super().__str__()}, width={self.width}, height={self.height}"
