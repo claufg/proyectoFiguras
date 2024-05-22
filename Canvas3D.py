cfcbb03fa9a8ccd5779906616d839b1368c403c2
claufg <168189721+claufg@users.noreply.github.com>
2024-05-22 18:40:28 +0200

    Update Canvas3D.py

--- Canvas3D.py
+++ Canvas3D.py
@@ -1,12 +1,40 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
 class Canvas:
-    # Código existente...
+    DEFAULT_SIZE = 1000.0
 
-    def plot(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        for shape in self.shapes:
-            shape.plot_3d(ax)
-        plt.show()
+    def __init__(self, title="default canvas", width=DEFAULT_SIZE, height=DEFAULT_SIZE):
+        if width < 0 or height < 0:
+            raise ValueError("Width and height cannot be negative")
+        self.title = title
+        self.width = width
+        self.height = height
+        self.forms = []
+
+    def add_form(self, form):
+        self.forms.append(form)
+
+    def clone(self):
+        new_canvas = Canvas(self.title, self.width, self.height)
+        new_canvas.forms = [form.clone() for form in self.forms]
+        return new_canvas
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+    def get_num_forms(self):
+        return len(self.forms)
+
+    def get_form(self, index):
+        if index < 1 or index > len(self.forms):
+            raise IndexError("Form index out of range")
+        return self.forms[index - 1]
+
+    def remove_form(self, index):
+        if index < 1 or index > len(self.forms):
+            raise IndexError("Form index out of range")
+        self.forms.pop(index - 1)
+
+    def __str__(self):
+        return f"{self.title} ({self.width},{self.height}) with {len(self.forms)} forms"
