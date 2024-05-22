4baed3a9c0e948a69353700da86ade156ee0a39c
claufg <168189721+claufg@users.noreply.github.com>
2024-05-22 13:22:26 +0200

    Update Canvas3D.py

--- Canvas3D.py
+++ Canvas3D.py
@@ -2,23 +2,11 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 class Canvas:
-    def __init__(self):
-        self.shapes = []
+    # Código existente...
 
-    def add_shape(self, shape):
-        self.shapes.append(shape)
-
-    def draw(self):
+    def plot(self):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         for shape in self.shapes:
-            position = shape.get_position()
-            if isinstance(shape, Circle3D):
-                ax.scatter(*position, marker='o', label='Circle')
-            elif isinstance(shape, Rectangle3D):
-                ax.scatter(*position, marker='s', label='Rectangle')
-            elif isinstance(shape, Square3D):
-                ax.scatter(*position, marker='^', label='Square')
-
-        plt.legend()
+            shape.plot_3d(ax)
         plt.show()
